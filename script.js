30354f2d5bfef94bbbeb78a2178587a288403a27
R. LaRoi <are@raewhoo.com>
2025-05-07 19:15:24 -0400

    Programs Checkbox Menu
    added program images for cards
    added functionality to program button which renders a dropdown menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-window.addEventListener("scroll", function () {
+//  nav scroll   --------
+window.addEventListener("scroll", () => {
   const navContainer = document.querySelector(".nav-container");
 
   if (window.scrollY > 100) {
@@ -6,4 +7,35 @@ window.addEventListener("scroll", function () {
   } else {
     navContainer.classList.remove("scrolled");
   }
+});
+
+//  programs dropdown menu ----------
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleCheckbox = document.getElementById("programs-dropdown-toggle");
+  const toggleButton = document.querySelector(".programs-button");
+
+  // show menu
+  toggleButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleCheckbox.checked = !toggleCheckbox.checked;
+  });
+
+  //close menu
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".programs-menu")) {
+      toggleCheckbox.checked = false;
+    }
+  });
+
+  // select programs
+  const programCheckboxes = document.querySelectorAll(".program-checkbox");
+
+  programCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
+      console.log(
+        checkbox.id + " is " + (checkbox.checked ? "checked" : "unchecked")
+      );
+    });
+  });
 });
